5df90d175501ac9c5436c20191827d691c936a95
Aman-Gupta0108 <amangupta.himt@gmail.com>
2025-09-29 08:34:40 +0530

    apply pannal animation

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,38 @@
+import { useRef, useState } from "react";
+import {useGSAP} from '@gsap/react';
+import gsap from "gsap";
+import 'remixicon/fonts/remixicon.css'
+
 export default function Home() {
+  const [pickup,setPickup]=useState('')
+  const [destination,setDestination]=useState('')
+  const [panelOpen,setPanelOpen] = useState(false)
+  const panelRef = useRef(null)
+  const panelCloseRef = useRef(null)
+  const[]=useState('')
+  const submitHandler = (e)=>{
+      e.preventDefault()
+      console.log(pickup)
+      console.log(destination)
+  }
+
+  useGSAP(function(){
+    if(panelOpen){
+      gsap.to(panelRef.current,{
+      height:'70%'
+    })
+    gsap.to(panelCloseRef.current,{
+      opacity:1
+    })
+    }else{
+       gsap.to(panelRef.current,{
+      height:'0%'
+    })
+     gsap.to(panelCloseRef.current,{
+      opacity:0
+    })
+    }
+  },[panelOpen])
   return (
     <>
       <div className="h-screen relative">
@@ -9,19 +43,43 @@ export default function Home() {
         />
 
         <div className="h-screen ">
-            <img className="h-full w-full object-cover" src="https://miro.medium.com/v2/resize:fit:1400/0*gwMx05pqII5hbfmX.gif" alt="" />
+          <img
+            className="h-full w-full object-cover"
+            src="https://miro.medium.com/v2/resize:fit:1400/0*gwMx05pqII5hbfmX.gif"
+            alt=""
+          />
         </div>
-        <div className="bg-white absolute buttom-0 w-full p-5">
-            <div className="" >
-                <h4 className="text-2xl font-semibold">Find a trip</h4>
-                <form action="">
-                    <input className="bg-[#eee] px-12 py-2 text-base rounded-lg w-full mt-5" type="text" placeholder="Add a Pick-up location" />
-                    <input className="bg-[#eee] px-12 py-2 text-base rounded-lg w-full mt-3" type="text" placeholder="Enter Your destination" />
-                </form>
-            </div>
-            <div className="h-0 "> 
+        <div className=" flex flex-col justify-end h-screen absolute top-0 w-full ">
+          <div className="h-[30%] bg-white p-6 relative">
+           <h5 ref={panelCloseRef} onClick={()=>setPanelOpen(false)} className="absolute opacity-0 top-6 right-6 text-2xl"> <i className="ri-arrow-down-wide-line"></i></h5>
+            <h4 className="text-2xl font-semibold">Find a trip</h4>
+            <form onSubmit={(e)=> submitHandler(e) }>
+              <div className="line absolute h-16 w-1 top-[45%] left-10 bg-gray-700 rounded-full"></div>
+              <input
+                className="bg-[#eee] px-12 py-2 text-lg rounded-lg w-full mt-5"
+                type="text"
+                placeholder="Add a Pick-up location"
+                value={pickup}
+                onClick={()=>{
+                  setPanelOpen(true)
+                }}
+                onChange={(e)=>setPickup(e.target.value)}
+              />
+              <input
+                className="bg-[#eee] px-12 py-2 text-lg rounded-lg w-full mt-3"
+                type="text"
+                placeholder="Enter Your destination"
+                onClick={()=>{
+                  setPanelOpen(true)
+                }}
+                value={destination}
+                onChange={(e)=>setDestination(e.target.value)}
+              />
+             
+            </form>
+          </div>
 
-            </div>
+          <div ref={panelRef} className=" bg-red-400 opacity-0  "></div>
         </div>
       </div>
     </>
