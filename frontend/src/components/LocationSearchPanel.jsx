795c304d3b562afb46ef56b716e14a7c61313f5c
amangupta-demo <amangupta.01008@gmail.com>
2025-09-29 18:29:37 +0530

    adding car and bike

---
+++ frontend/src/components/LocationSearchPanel.jsx
@@ -0,0 +1,25 @@
+import React from "react"
+const LocationSearchPanel = () =>{
+    return(
+        <div> 
+        {/* this is just a sample data  */}
+        <div className="flex gap-4 items-center my-4 justify-start">
+            <h2 className="bg-[#eee] h-8 w-12 flex items-center justify-center rounded-full"><i className="ri-map-pin-fill"></i></h2>
+            <h4 className="font-medium">noida sector 122 noida sector 122 noida sector 122</h4>
+        </div>
+
+ <div className="flex gap-4 items-center my-4 justify-start">
+            <h2 className="bg-[#eee] h-8 w-12 flex items-center justify-center rounded-full"><i className="ri-map-pin-fill"></i></h2>
+            <h4 className="font-medium">noida sector 122 noida sector 122 noida sector 122</h4>
+        </div>
+ 
+  <div className="flex gap-4 items-center my-4 justify-start">
+            <h2 className="bg-[#eee] h-8 w-12 flex items-center justify-center rounded-full"><i className="ri-map-pin-fill"></i></h2>
+            <h4 className="font-medium">noida sector 122 noida sector 122 noida sector 122</h4>
+        </div>
+
+
+        </div>
+    )
+}
+export default LocationSearchPanel;
